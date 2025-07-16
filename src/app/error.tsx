5fc63fe7c3deb571b1c7d19a8107e283cbc6f300
bit-pal <akioito217@gmail.com>
2025-07-16 18:23:14 +0900

    icon error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,7 @@
 'use client'; // Error components must be Client Components
 
 import * as React from 'react';
-import { RiAlarmWarningFill } from 'react-icons/ri';
+import { ShieldAlert } from 'lucide-react';
 
 
 export default function Error({
@@ -20,7 +20,7 @@ export default function Error({
     <main>
       <section className='bg-white'>
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-          <RiAlarmWarningFill
+          <ShieldAlert
             size={60}
             className='drop-shadow-glow animate-flicker text-red-500'
           />
